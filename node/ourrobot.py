ab43a616b6a1b689f35edac0dce4184e65d192e9
Jackuzzi003 <yannickweitz@yahoo.de>
2017-03-21 17:29:57 +0100

    start and stop added

--- node/ourrobot.py
+++ node/ourrobot.py
@@ -4,22 +4,29 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 def position():
-    
-        rospy.loginfo('please enter')
-        x = raw_input()
+        print('please enter start or stop')
+        y = raw_input()
         rospy.init_node('ourrobot')
         pub = rospy.Publisher('/cmd_vel',Twist,queue_size = 10)
-        
-        twist = Twist() 
-        
-        if x == 'left':
-            twist.linear.y= 0.25
-            rospy.loginfo('left')
-        elif x == 'right':
-            twist.linear.y = -0.25
-            rospy.loginfo('right')
-        pub.publish(twist)
-    
+        twist = Twist()
+        if y == 'start':
+            print('please enter left or right')
+            x = raw_input()
+            
+            
+            if x == 'left':
+                twist.linear.y= 0.5
+                print('left')
+                
+            elif x == 'right':
+                twist.linear.y = -0.5
+                print('right')
+                
+            pub.publish(twist)
+        elif y =='stop':
+            
+            twist.linear.y = 0.0
+            pub.publish(twist)
         
     
 if __name__ == '__main__':
