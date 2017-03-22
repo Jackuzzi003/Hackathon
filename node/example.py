b765ac495d3d35a35d9662b9a12abef97742faa0
Jackuzzi003 <yannickweitz@yahoo.de>
2017-03-22 19:38:24 +0100

    added comments to example.py

--- node/example.py
+++ node/example.py
@@ -9,28 +9,26 @@ class example(object):
     #constructor
     def __init__(self):
         pass
-    
-    def position(self):
-        rospy.Subscriber("/odom",Odometry,self.call)
-        y = raw_input('please enter start or stop: ')
-        pub = rospy.Publisher('/cmd_vel',Twist,queue_size = 10)
-        a = self.b
-        twist = Twist()
-        if y == 'start':
-            x = raw_input('please enter left or right: ')
-            z = raw_input('please enter velocity: ')
-            d = raw_input('please enter distance: ')
-            d = float(d)
-            if x == 'left':
-                while self.b < a+d:
-                    print("self.b")
-                    twist.linear.y= float(z)
-                    print('left')
-                    pub.publish(twist)
-                twist.linear.y = 0.0
+    # Function to controll the Robot
+    def position(self): 
+        rospy.Subscriber("/odom",Odometry,self.call)                            # subscribe to the Odometry topic
+        y = raw_input('please enter start or stop: ')                           # get Input for starting and stopping, not really needed in the final program, but here it is left to give the Subscriber some time, otherwise it would give an error
+        pub = rospy.Publisher('/cmd_vel',Twist,queue_size = 10)                 # initializing pub to be a Publisher to the cmd_vel topic
+        a = self.b                                                              # saving the current position for distance measurement
+        twist = Twist()                                                         # creating the Twist variable to publish the velocity later
+        if y == 'start':                                                        
+            x = raw_input('please enter left or right: ')                       # getting Input for the direction to move 
+            z = raw_input('please enter velocity: ')                            # getting the velocity at which the robot should move
+            d = raw_input('please enter distance: ')                            # getting the distance 
+            d = float(d)                                                        # converting str(d) to float(d)
+            if x == 'left':                                                         
+                while self.b < a+d:                                             # run this as long as the y value of the robots current position is smaller than the starting position added to the wanted distance
+                    twist.linear.y= float(z)    
+                    pub.publish(twist)                                          # publishing the velocity command to the cmd_vel topic
+                twist.linear.y = 0.0                                            # stopping the robot after the distance has been driven
                 pub.publish(twist)
                         
-            elif x == 'right':
+            elif x == 'right':                                                  # same as 'left' only for 'right', so the signs have to be inverted
                 while self.b > a-d:
                     twist.linear.y = -float(z)
                     print(' b ' + str(self.b) + ' pos ' + str(a-d))
@@ -38,26 +36,20 @@ class example(object):
                 twist.linear.y = 0.0
                 pub.publish(twist) 
                 
-        elif y =='stop':
+        elif y =='stop':                                                        # stop condition is not needed
             
             twist.linear.y = 0.0
             pub.publish(twist)
-        print("done")
+        print("done")    
         
-    def function(self):
-        pass
-        #rospy.loginfo(self.b)
-        #rospy.loginfo(self.b)
-        
-        
-    def call(self,msg):
-        self.b = msg.pose.pose.position.y
+    def call(self,msg):                                                         
+        self.b = msg.pose.pose.position.y                                       # getting the y value of the robots position from the topic /odom and assigning it to b
         
         
     
 if __name__ == '__main__':
     rospy.init_node('anyname')
-    obj1 = example()
+    obj1 = example()                                                            # creating an object of the class that has been made
     while not rospy.is_shutdown():
-        obj1.position()
+        obj1.position()                                                         # running the function to move the robot
         #rospy.spin()
